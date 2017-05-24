98abcaa122563f7bef06475933713b2aae2c0722
Rubertavio <ruberpalomino1@gmail.com>
2017-05-24 00:10:03 -0500

    v1.1

---
+++ app/src/main/java/com/example/enrique/petagram/DetalleMascota.java
@@ -0,0 +1,19 @@
+package com.example.enrique.petagram;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Enrique on 23/05/2017.
+ */
+
+public class DetalleMascota extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.mascota_detalle);
+    }
+}
